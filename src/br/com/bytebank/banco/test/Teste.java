dabe940cabb9f15b78730c8a7d021791b117584d
Guilherme Henrique <66800181+guiLira-Dev@users.noreply.github.com>
2021-03-21 16:06:21 -0300

    Update Teste.java

--- src/br/com/bytebank/banco/test/Teste.java
+++ src/br/com/bytebank/banco/test/Teste.java
@@ -8,10 +8,6 @@ import br.com.bytebank.banco.modelo.ContaPoupanca;
 public class Teste {
 
 	public static void main(String[] args) {
-
-//		System.out.println("x");
-//		System.out.println(3);
-//		System.out.println(false);
 		
 		Object cc = new ContaCorrente(22, 33);
 		Object cp = new ContaPoupanca(33, 22);
@@ -20,7 +16,6 @@ public class Teste {
 		System.out.println(cc);
 		System.out.println(cp);
 		
-		//println(cliente);
 	}
 	
 	static void println() {}
